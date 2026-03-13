cae1c78d8d9e046e28612866433c4358491bc272
w enxig <wenxinguo12@yeah.net>
2026-03-13 22:13:45 +0800

    chore: 初始化项目

---
+++ packages/app/electron.vite.config.ts
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'electron-vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import VueRouter from 'vue-router/vite'
+
+export default defineConfig({
+  main: { resolve: { alias: { '@': fileURLToPath(new URL('src/main/src', import.meta.url)) } } },
+  preload: {
+    resolve: { alias: { '@': fileURLToPath(new URL('src/preload/src', import.meta.url)) } }
+  },
+  renderer: {
+    plugins: [
+      VueRouter({ dts: 'typed-router.d.ts' }),
+      vue(),
+      vueJsx(),
+      Components({ dts: true, resolvers: [NaiveUiResolver()] }),
+      tailwindcss()
+    ],
+    css: { transformer: 'lightningcss', lightningcss: {} },
+    resolve: { alias: { '@': fileURLToPath(new URL('src/renderer/src', import.meta.url)) } }
+  }
+})
